3b5b6db4253e99609eb4c732b955679bdc854d44
Olatunji Olayinka <olatunji.weber@gmail.com>
2022-06-26 04:04:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const skillsList = ["Python", "Django", "HTML", "CSS", "JavaScript", "Nodejs", "Java", "MySQL", "Google Cloud", "Canva"];
 var items = ["Buy Food", "Eat Food", "Cook Food"];
 var workItems = [];
+var date = require(__dirname + "/date.js");
 
 const app = express();
 app.set("view engine", "ejs");  //get our nodejs app to use ejs templating
@@ -10,25 +12,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    var day = today.toLocaleDateString("en-US", options);
+    let day = date.getDate();
     res.render('list', {listTitle: day, newListItems: items});
 })
 
 app.post("/", function(req, res){
     let item = req.body.newTask;
-    if(req.body.listTitle === "Work"){
+    if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work");
     }else{
         items.push(item);
         res.redirect("/");
     }
+});
+
+app.get("/about", function(req, res){
+    res.render('about', {listTitle: "About the Developer", newListItems: skillsList});
 });
 
 app.get("/work", function(req, res){
